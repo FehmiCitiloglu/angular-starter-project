82d71426aff957bee96b13b32d6b06673e84c489
FehmiCitiloglu <fehmicitiloglu@gmail.com>
2022-04-15 21:12:54 +0300

    first cmmit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,7 +19,7 @@ describe('AppComponent', () => {
   it(`should have as title 'my-first-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-first-app');
+    expect(app.name).toEqual('Fehmi');
   });
 
   it('should render title', () => {
---
+++ src/app/server/server.component.ts
@@ -0,0 +1,13 @@
+
+
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-server',
+    templateUrl: './server.component.html'
+})
+
+
+export class ServerComponent {
+
+ }
