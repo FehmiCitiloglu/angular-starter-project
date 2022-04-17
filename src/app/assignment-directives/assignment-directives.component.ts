0bffae7bc5448c7002cfab22da34db11616773f9
FehmiCitiloglu <fehmicitiloglu@gmail.com>
2022-04-17 22:53:10 +0300

    directives assignment instructure solution

--- src/app/assignment-directives/assignment-directives.component.ts
+++ src/app/assignment-directives/assignment-directives.component.ts
@@ -7,11 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AssignmentDirectivesComponent implements OnInit {
   showParagraph = false
-  buttonLogs: string[] = []
+  buttonLogs: number[] = []
   constructor() { }
 
   toggleParagraph() {
-    this.buttonLogs.push(new Date().getTime().toString())
+    this.buttonLogs.push(this.buttonLogs.length + 1)
     this.showParagraph = !this.showParagraph
   }
   ngOnInit(): void {
